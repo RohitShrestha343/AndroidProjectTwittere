cbaf54704faa083e7c857a33f6d8f44e7efc50cf
Rohit Shrestha <rohit.xresta@gmail.com>
2020-01-03 22:30:17 +0545

    Adapter,Api,Model,Url

---
+++ app/src/main/java/com/shrestharohit/androidprojecttwittere/api/API.java
@@ -0,0 +1,44 @@
+package com.shrestharohit.androidprojecttwittere.api;
+
+
+import com.shresthagaurav.androidprojecttwitter.model.Check;
+import com.shresthagaurav.androidprojecttwitter.model.ImageModel;
+import com.shresthagaurav.androidprojecttwitter.model.SignUpResponse;
+import com.shresthagaurav.androidprojecttwitter.model.TweetM;
+import com.shresthagaurav.androidprojecttwitter.model.User;
+import com.shresthagaurav.androidprojecttwitter.model.UserInfo;
+
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface API {
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<SignUpResponse> checkUser(@Field("email") String username, @Field("password") String password);
+
+
+    @POST("users/signup")
+    Call<SignUpResponse> register(@Body User cud);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageModel> uploadImage(@Part MultipartBody.Part imageFile);
+
+    @POST("users/check")
+    Call<Check> check(@Body User email);
+
+    @POST("users/showalltweet")
+    Call<List<TweetM>> GetTweet(@Header("Authorization") String token);
+
+    @POST("users/me")
+    Call<UserInfo> getUser(@Header("Authorization") String token);
+}
---
+++ app/src/main/java/com/shrestharohit/androidprojecttwittere/model/Check.java
@@ -0,0 +1,13 @@
+package com.shrestharohit.androidprojecttwittere.model;
+
+public class Check {
+ private    String status;
+
+    public Check(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
